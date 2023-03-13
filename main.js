30493287b3428de3d0672bb8da2f9620cc239b34
Kevin Ramirez <kevinrave1804@gmail.com>
2023-03-13 15:01:52 -0500

    Version JS full

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 const SelectHV=document.querySelector("#HV")
 const HojaVida=document.querySelector(".HojaVida")
 
+const Portafolio=document.querySelector("#Portafolio")
+
 const Inicio=document.querySelector(".Inicio")
 
 SelectHV.addEventListener("click",HV)
+Portafolio.addEventListener("click",IniPortafolio)
 
 function HV(){
     const InicioClosed=Inicio.classList.contains("inactive")
+
     if(!InicioClosed){
         Inicio.classList.add("inactive")
-    }
+    }    
 
     HojaVida.classList.toggle("inactive")
+}
+
+function IniPortafolio(){
+    const HojaVidaClosed=HojaVida.classList.contains("inactive")
+
+    if(!HojaVidaClosed){
+        HojaVida.classList.add("inactive")
+    }
+
+    Inicio.classList.toggle("inactive")
 }
