c6ff778bfe08e4985c01ad68afcd12bd54a87914
Kevin Ramirez <kevinrave1804@gmail.com>
2023-06-02 13:20:58 -0500

    configuracion con APIftech

---
+++ catalogo.js
@@ -0,0 +1,29 @@
+const cargar = (document.querySelector("body").onload = function() {
+    const p1 = producto();
+});
+const productos=document.getElementById("catalogoPro")
+
+function producto(){
+    fetch("./catalogo.json")
+    .then((res)=>res.json())
+    .then((data)=>{
+        console.log(data);
+        data.catalogo.forEach(element => {
+            productos.innerHTML+=`
+            <li>
+                <img src="${element.producto.fotos}" alt="${element.producto.nombre}">
+                <h3>
+                    ${element.producto.nombre}  (Referencia:${element.producto.referencia})
+                </h3>
+                <p>
+                    ${element.producto.descripcion}
+                </p>
+                <div>
+                    <b>${element.producto.precio}</b>
+                    <button>Compra</button>
+                </div>
+            </li>
+            `
+        });
+    })
+}
